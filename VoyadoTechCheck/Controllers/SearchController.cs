7bc52bbd8d7f95bbbffb215331372ce841e74546
patrikpekkala <patrik.pekkala@gmail.com>
2022-03-15 21:19:08 +0100

    adding files

---
+++ VoyadoTechCheck/Controllers/SearchController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VoyadoTechCheck.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public double Post([FromForm] string searchText)
+        {
+            string[] searchArr = searchText.Split(" ");
+
+            SearchEngineHits search = new SearchEngineHits();
+
+            search.SearchForHits(searchArr);
+
+            return search.TotalHits;
+        }
+
+    }
+}
---
+++ VoyadoTechCheck/Services/SearchEngineHits.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+namespace VoyadoTechCheck.Services
+{
+    public class SearchEngineHits
+    {
+        public double TotalHits { get; set; }
+        static string bingSubscriptionKey = Environment.GetEnvironmentVariable("BING_SEARCH_V7_SUBSCRIPTION_KEY");
+        static string bingEndpoint = Environment.GetEnvironmentVariable("BING_SEARCH_V7_ENDPOINT") + "/v7.0/search";
+        static string rapidKey = Environment.GetEnvironmentVariable("RAPID_KEY");
+        static string rapidEndpoint = Environment.GetEnvironmentVariable("RAPID_ENDPOINT") + "/api/Search/WebSearchAPI";
+
+        public void SearchForHits(string[] words)
+        {
+            TotalHits = 0;
+            //call search engines with all words in array
+            foreach (string word in words)
+            {
+                SearchRapid(word);
+                SearchBing(word);
+            }
+        }
+        private async void SearchBing(string searchWord)
+        {
+            var uriQuery = bingEndpoint + "?q=" + Uri.EscapeDataString(searchWord);
+
+
+            WebRequest request = HttpWebRequest.Create(uriQuery);
+            request.Headers["Ocp-Apim-Subscription-Key"] = bingSubscriptionKey;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            var webpages = parsedJson.webPages;
+
+            TotalHits = TotalHits + double.Parse((webpages.totalEstimatedMatches).ToString());
+            //Console.WriteLine(JsonConvert.SerializeObject(parsedJson, Formatting.Indented));
+        }
+
+        private async void SearchRapid(string searchWord)
+        {
+            var uriQuery = rapidEndpoint + "?q=" + Uri.EscapeDataString(searchWord);
+
+
+            WebRequest request = HttpWebRequest.Create(uriQuery);
+            request.Headers["x-rapidapi-key"] = rapidKey;
+            request.Headers["x-rapidapi-host"] = "contextualwebsearch-websearch-v1.p.rapidapi.com";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+
+            TotalHits = TotalHits + double.Parse((parsedJson.totalCount).ToString());
+            //Console.WriteLine(JsonConvert.SerializeObject(parsedJson, Formatting.Indented));
+        }
+    }
+}
